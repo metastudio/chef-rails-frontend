71eb58f3cc54c1b38d9d43e40fb0c9b2743930b0
Andrey Chernih <andrey.chernih@gmail.com>
2013-05-28 14:38:41 +0400

    Setup some other defaults

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,6 @@ default[:nginx][:client_max_body_size] = '100m'
 default[:nginx][:init_style] = 'init'
 
 default[:application][:name] = 'example.com'
+default[:application][:socket] = '/tmp/example.com'
+default[:application][:url] = 'example.com'
 default[:application][:redirect_urls] = []
